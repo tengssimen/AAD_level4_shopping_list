110d49f12a8330bfa73ab1d427b5a2d6d9e2c133
tengssimen <simentengs@gmail.com>
2019-10-10 16:25:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglist/database/ProductDao.kt
@@ -0,0 +1,24 @@
+package com.example.shoppinglist.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.shoppinglist.model.Product
+
+@Dao
+interface ProductDao {
+
+    @Query("SELECT * FROM product_table")
+    suspend fun getAllProducts(): List<Product>
+
+    @Insert
+    suspend fun insertProduct(product: Product)
+
+    @Delete
+    suspend fun deleteProduct(product: Product)
+
+    @Query("DELETE FROM product_table")
+    suspend fun deleteAllProducts()
+
+}
---
+++ app/src/main/java/com/example/shoppinglist/database/ProductRepository.kt
@@ -0,0 +1,25 @@
+package com.example.shoppinglist.database
+
+import android.content.Context
+import com.example.shoppinglist.model.Product
+
+
+class ProductRepository(context: Context) {
+
+    private val productDao: ProductDao
+
+    init {
+        val database =
+            ShoppingListRoomDatabase.getDatabase(context)
+        productDao = database!!.productDao()
+    }
+
+    suspend fun getAllProducts(): List<Product> = productDao.getAllProducts()
+
+    suspend fun insertProduct(product: Product) = productDao.insertProduct(product)
+
+    suspend fun deleteProduct(product: Product) = productDao.deleteProduct(product)
+
+    suspend fun deleteAllProducts() = productDao.deleteAllProducts()
+
+}
